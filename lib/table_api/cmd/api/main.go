5a93b2d9c526f5f983ae3dfd617e06598fe64f64
cobbinma <matthewc@nandos.co.uk>
2020-07-18 08:18:06 +0100

    install echo router

--- lib/table_api/cmd/api/main.go
+++ lib/table_api/cmd/api/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"github.com/cobbinma/booking/lib/table_api/config"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+)
+
 func main() {
-	println("HELLO")
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+
+	e.GET("/healthz", health)
+
+	e.Logger.Fatal(e.Start(config.Port()))
+}
+
+func health(c echo.Context) error {
+	return c.NoContent(http.StatusOK)
 }
--- lib/table_api/go.mod
+++ lib/table_api/go.mod
@@ -1,3 +1,5 @@
 module github.com/cobbinma/booking/lib/table_api
 
 go 1.14
+
+require github.com/labstack/echo/v4 v4.1.16
