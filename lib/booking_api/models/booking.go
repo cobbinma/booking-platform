ae52f173ddbdc445ddccfc2866422e4d75d16278
cobbinma <matthewc@nandos.co.uk>
2020-07-18 13:48:08 +0100

    create booking type

--- lib/booking_api/models/booking.go
+++ lib/booking_api/models/booking.go
@@ -1,1 +1,10 @@
 package models
+
+type Booking struct {
+	ID         int    `json:"id" db:"id"`
+	CustomerID string `json:"customer_id" db:"customer_id"`
+	TableID    int    `json:"table_id" db:"table_id"`
+	People     int    `json:"people" db:"people"`
+	StartsAt   string `json:"starts_at" db:"starts_at"`
+	EndsAt     string `json:"ends_at" db:"ends_at"`
+}
