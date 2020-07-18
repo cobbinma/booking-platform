cc1f0a554db9b6081ff18ada715a79547f853e89
cobbinma <matthewc@nandos.co.uk>
2020-07-18 13:37:47 +0100

    add people to booking

--- lib/booking_api/migrations/1_initialize_schema.up.sql
+++ lib/booking_api/migrations/1_initialize_schema.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS bookings (
     id  SERIAL UNIQUE PRIMARY KEY,
     customer_id VARCHAR NOT NULL,
     table_id INTEGER NOT NULL,
+    people  INTEGER NOT NULL,
     starts_at TIMESTAMP NOT NULL,
     ends_at TIMESTAMP NOT NULL
 );
