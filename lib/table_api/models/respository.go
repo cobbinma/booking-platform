02bc5d1b6878381e67adc4b36e4bc3b8fd3b2053
cobbinma <matthewc@nandos.co.uk>
2020-07-18 09:30:19 +0100

    define repository interface

---
+++ lib/table_api/models/respository.go
@@ -0,0 +1,9 @@
+package models
+
+import "context"
+
+type Repository interface {
+	CreateTable(ctx context.Context, newTable NewTable) error
+	GetTables(ctx context.Context, options func(options *GetTablesOptions) *GetTablesOptions) ([]Table, error)
+	DeleteTable(ctx context.Context, id TableID) error
+}
--- lib/table_api/models/table.go
+++ lib/table_api/models/table.go
@@ -1,12 +1,29 @@
 package models
 
 type Table struct {
-	id       string
-	name     string
+	ID       TableID `json:"id"`
+	Name     string  `json:"name"`
+	Capacity int     `json:"capacity"`
+}
+
+type TableID string
+
+type NewTable struct {
+	Name     string `json:"name"`
+	Capacity int    `json:"capacity"`
+}
+
+type GetTablesOptions struct {
 	capacity int
 }
 
-type NewTable struct {
-	name     string
-	capacity int
+func NewGetTablesOptions() *GetTablesOptions {
+	return &GetTablesOptions{}
+}
+
+func WithCapacity(capacity int) func(options *GetTablesOptions) *GetTablesOptions {
+	return func(options *GetTablesOptions) *GetTablesOptions {
+		options.capacity = capacity
+		return options
+	}
 }
