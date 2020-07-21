64ea16fb9bf1c4a5599fb5cc87087020560d7903
cobbinma <matthewc@nandos.co.uk>
2020-07-21 12:38:07 +0100

    react router and book page

--- lib/booking-ui/src/App.tsx
+++ lib/booking-ui/src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import NavBar from "./components/layout/AppBar";
+import { BrowserRouter, Route } from "react-router-dom";
+import Book from "./components/pages/Book";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <h1>Hi</h1>
+      <BrowserRouter>
+        <NavBar />
+        <Route exact={true} path="/" component={Home} />
+        <Route path="/book" component={Book} />
+      </BrowserRouter>
     </div>
   );
 }
