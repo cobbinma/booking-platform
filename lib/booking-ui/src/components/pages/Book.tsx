a889ea5496c995e7c7b3804d01794da7d2cd61d2
cobbinma <cobbinma@gmail.com>
2020-07-22 21:26:46 +0100

    remove skip step handling

--- lib/booking-ui/src/components/pages/Book.tsx
+++ lib/booking-ui/src/components/pages/Book.tsx
@@ -52,7 +52,6 @@ const Book = () => {
   });
   const [bookingSlot, setBookingSlot] = React.useState<BookingSlot | null>();
   const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set<number>());
   const steps = getSteps();
 
   const getStepContent = (step: number) => {
@@ -108,19 +107,8 @@ const Book = () => {
     }
   };
 
-  const isStepSkipped = (step: number) => {
-    return skipped.has(step);
-  };
-
   const handleNext = () => {
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
-    }
-
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped(newSkipped);
   };
 
   const handleBack = () => {
@@ -134,9 +122,6 @@ const Book = () => {
         {steps.map((label, index) => {
           const stepProps: { completed?: boolean } = {};
           const labelProps: { optional?: React.ReactNode } = {};
-          if (isStepSkipped(index)) {
-            stepProps.completed = false;
-          }
           return (
             <Step key={label} {...stepProps}>
               <StepLabel {...labelProps}>{label}</StepLabel>
