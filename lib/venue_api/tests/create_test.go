b560c6fdbbbf1355a478cc313ca08f7a8932005e
cobbinma <cobbinma@gmail.com>
2020-09-22 19:15:02 +0100

    get venue and delete venue integration tests

--- lib/venue_api/tests/create_test.go
+++ lib/venue_api/tests/create_test.go
@@ -19,6 +19,7 @@ import (
 	"net/url"
 	"os"
 	"runtime"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -183,6 +184,142 @@ func TestCreateVenue(t *testing.T) {
 
 	if vr.OpeningHours[0].Closes != closes {
 		t.Errorf("closes was '%s', expected '%s'", vr.OpeningHours[0].Closes, closes)
+	}
+}
+
+func TestCreateGetVenue(t *testing.T) {
+	name := "kings arms"
+	day := 2
+	opens := "08:00"
+	closes := "21:00"
+	venueJSON := fmt.Sprintf(`{"name":"%s","openingHours":[{"dayOfWeek":%v,"opens":"%s","closes":"%s"}]}`, name, day, opens, closes)
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(venueJSON))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	createVenue := handlers.CreateVenue(repository)
+	err := createVenue(c)
+	if err != nil {
+		t.Errorf("error returned from create venue handler : %s", err)
+		return
+	}
+
+	if rec.Code != http.StatusCreated {
+		t.Errorf("response code '%v' was not expected '%v'", rec.Code, http.StatusCreated)
+		return
+	}
+
+	var vr venueResponse
+	if err := json.Unmarshal(rec.Body.Bytes(), &vr); err != nil {
+		t.Errorf("could not unmarshall response : %s", err)
+		return
+	}
+
+	id := vr.ID
+	vr = venueResponse{}
+
+	req = httptest.NewRequest(http.MethodGet, "/", nil)
+	rec = httptest.NewRecorder()
+	c = e.NewContext(req, rec)
+	c.SetPath("/venues/:id")
+	c.SetParamNames("id")
+	c.SetParamValues(strconv.Itoa(id))
+
+	getVenue := handlers.GetVenue(repository)
+	if err := getVenue(c); err != nil {
+		t.Errorf("error returned from get venue handler : %s", err)
+		return
+	}
+
+	if err := json.Unmarshal(rec.Body.Bytes(), &vr); err != nil {
+		t.Errorf("could not unmarshall response : %s", err)
+		return
+	}
+
+	if vr.Name != name {
+		t.Errorf("name was '%s', expected '%s'", vr.Name, name)
+	}
+
+	if vr.OpeningHours[0].DayOfWeek != day {
+		t.Errorf("day of week was '%v', expected '%v'", vr.OpeningHours[0].DayOfWeek, day)
+	}
+
+	if vr.OpeningHours[0].Opens != opens {
+		t.Errorf("opens was '%s', expected '%s'", vr.OpeningHours[0].Opens, opens)
+	}
+
+	if vr.OpeningHours[0].Closes != closes {
+		t.Errorf("closes was '%s', expected '%s'", vr.OpeningHours[0].Closes, closes)
+	}
+}
+
+func TestCreateDeleteGetVenue(t *testing.T) {
+	name := "case is altered"
+	day := 1
+	opens := "10:00"
+	closes := "23:00"
+	venueJSON := fmt.Sprintf(`{"name":"%s","openingHours":[{"dayOfWeek":%v,"opens":"%s","closes":"%s"}]}`, name, day, opens, closes)
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(venueJSON))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	createVenue := handlers.CreateVenue(repository)
+	err := createVenue(c)
+	if err != nil {
+		t.Errorf("error returned from create venue handler : %s", err)
+		return
+	}
+
+	if rec.Code != http.StatusCreated {
+		t.Errorf("response code '%v' was not expected '%v'", rec.Code, http.StatusCreated)
+		return
+	}
+
+	var vr venueResponse
+	if err := json.Unmarshal(rec.Body.Bytes(), &vr); err != nil {
+		t.Errorf("could not unmarshall response : %s", err)
+		return
+	}
+
+	id := vr.ID
+	vr = venueResponse{}
+
+	req = httptest.NewRequest(http.MethodDelete, "/", nil)
+	rec = httptest.NewRecorder()
+	c = e.NewContext(req, rec)
+	c.SetPath("/venues/:id")
+	c.SetParamNames("id")
+	c.SetParamValues(strconv.Itoa(id))
+
+	deleteVenue := handlers.DeleteVenue(repository)
+	if err := deleteVenue(c); err != nil {
+		t.Errorf("error returned from get venue handler : %s", err)
+		return
+	}
+
+	if rec.Code != http.StatusOK {
+		t.Errorf("response code '%v' was not expected '%v'", rec.Code, http.StatusOK)
+	}
+
+	req = httptest.NewRequest(http.MethodGet, "/", nil)
+	rec = httptest.NewRecorder()
+	c = e.NewContext(req, rec)
+	c.SetPath("/venues/:id")
+	c.SetParamNames("id")
+	c.SetParamValues(strconv.Itoa(id))
+
+	getVenue := handlers.GetVenue(repository)
+	if err := getVenue(c); err != nil {
+		t.Errorf("error returned from get venue handler : %s", err)
+		return
+	}
+
+	if rec.Code != http.StatusNotFound {
+		t.Errorf("response code '%v' was not expected '%v'", rec.Code, http.StatusNotFound)
 	}
 }
 
