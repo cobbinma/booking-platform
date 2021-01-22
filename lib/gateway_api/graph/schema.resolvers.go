217debfebcc08779675d29007c28c356a9683820
cobbinma <cobbinma@gmail.com>
2021-01-22 19:43:58 +0000

    user email has to match auth email

--- lib/gateway_api/graph/schema.resolvers.go
+++ lib/gateway_api/graph/schema.resolvers.go
@@ -30,9 +30,13 @@ func (r *mutationResolver) CreateSlot(ctx context.Context, input models.SlotInpu
 }
 
 func (r *mutationResolver) CreateBooking(ctx context.Context, input models.BookingInput) (*models.Booking, error) {
-	_, err := models.UserFromContext(ctx)
+	user, err := models.UserFromContext(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("could not get user from context : %w", err)
+	}
+
+	if user.Email != input.CustomerID {
+		return nil, fmt.Errorf("context email does not match given")
 	}
 
 	starts, err := input.StartsAt.Time()
