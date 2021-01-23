b70cfff999c4ac12a7b6d2f73389c1ae36dd8481
cobbinma <cobbinma@gmail.com>
2021-01-23 18:04:31 +0000

    return dummy venue data

--- lib/venue_api/cmd/api/main.go
+++ lib/venue_api/cmd/api/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"github.com/cobbinma/booking-platform/lib/protobuf/autogen/lang/go/venue/api"
+	"github.com/cobbinma/booking-platform/lib/protobuf/autogen/lang/go/venue/models"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"net"
@@ -22,10 +24,60 @@ func main() {
 	}
 
 	s := grpc.NewServer()
-	api.RegisterVenueAPIServer(s, &api.UnimplementedVenueAPIServer{})
+	api.RegisterVenueAPIServer(s, &Service{})
 
 	log.Infof("starting gRPC listener on port %s", port)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve : %s", err)
 	}
+}
+
+var _ api.VenueAPIServer = (*Service)(nil)
+
+type Service struct{}
+
+func (s Service) GetVenue(ctx context.Context, request *api.GetVenueRequest) (*models.Venue, error) {
+	hours := []*models.OpeningHoursSpecification{
+		{
+			DayOfWeek: 1,
+			Opens:     "10:00",
+			Closes:    "19:00",
+		},
+		{
+			DayOfWeek: 2,
+			Opens:     "10:00",
+			Closes:    "19:00",
+		},
+		{
+			DayOfWeek: 3,
+			Opens:     "10:00",
+			Closes:    "19:00",
+		},
+		{
+			DayOfWeek: 4,
+			Opens:     "10:00",
+			Closes:    "19:00",
+		},
+		{
+			DayOfWeek: 5,
+			Opens:     "10:00",
+			Closes:    "19:00",
+		},
+		{
+			DayOfWeek: 6,
+			Opens:     "10:00",
+			Closes:    "19:00",
+		},
+		{
+			DayOfWeek: 7,
+			Opens:     "10:00",
+			Closes:    "19:00",
+		},
+	}
+	return &models.Venue{
+		Id:                  request.Id,
+		Name:                "Hop and Vine",
+		OpeningHours:        hours,
+		SpecialOpeningHours: nil,
+	}, nil
 }
