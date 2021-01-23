6c490666c271138d76e6a50772264503c12def32
cobbinma <cobbinma@gmail.com>
2021-01-23 17:57:20 +0000

    import protobuf and create venue gRPC server

--- lib/venue_api/cmd/api/main.go
+++ lib/venue_api/cmd/api/main.go
@@ -1,41 +1,31 @@
 package main
 
 import (
-	"context"
-	"github.com/cobbinma/booking-platform/lib/venue_api/cmd/api/handlers"
-	"github.com/cobbinma/booking-platform/lib/venue_api/config"
-	"github.com/cobbinma/booking-platform/lib/venue_api/repositories/postgres"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/cobbinma/booking-platform/lib/protobuf/autogen/lang/go/venue/api"
 	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"net"
+	"os"
 )
 
 func main() {
 	log.SetFormatter(&log.JSONFormatter{})
 
-	dbClient, closeDB, err := postgres.NewDBClient(config.PostgresURL())
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8888"
+	}
+
+	lis, err := net.Listen("tcp", ":"+port)
 	if err != nil {
-		log.Fatal("could not create database client : ", err)
-	}
-	defer func() {
-		if err := closeDB(); err != nil {
-			log.Error("could not close database : ", err)
-		}
-	}()
-
-	repository := postgres.NewPostgres(dbClient)
-	if err := repository.Migrate(context.Background(), "file://migrations"); err != nil {
-		log.Fatal("could not migrate : ", err)
+		log.Fatalf("could not listen : %s", err)
 	}
 
-	e := echo.New()
+	s := grpc.NewServer()
+	api.RegisterVenueAPIServer(s, &api.UnimplementedVenueAPIServer{})
 
-	e.Use(middleware.Logger())
-
-	e.GET("/healthz", handlers.Health)
-	e.POST("/venues", handlers.CreateVenue(repository))
-	e.GET("/venues/:id", handlers.GetVenue(repository))
-	e.DELETE("/venues/:id", handlers.DeleteVenue(repository))
-
-	e.Logger.Fatal(e.Start(config.Port()))
+	log.Infof("starting gRPC listener on port %s", port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve : %s", err)
+	}
 }
