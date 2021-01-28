c9644e044ca7cce4d50e12cca2e4ffb80b7240d9
cobbinma <cobbinma@gmail.com>
2021-01-28 18:04:18 +0000

    add schema and run database migration

--- lib/venue_api/internal/postgres/client.go
+++ lib/venue_api/internal/postgres/client.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"github.com/cobbinma/booking-platform/lib/protobuf/autogen/lang/go/venue/api"
 	"github.com/cobbinma/booking-platform/lib/protobuf/autogen/lang/go/venue/models"
+	"github.com/golang-migrate/migrate/v4"
+	pgres "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"go.uber.org/zap"
@@ -54,6 +57,10 @@ func NewPostgres(log *zap.SugaredLogger, options ...func(*client)) (Repository, 
 	}
 	c.db = db
 
+	if err := c.migrate(); err != nil {
+		return nil, nil, fmt.Errorf("could not migrate : %w", err)
+	}
+
 	return c, func(log *zap.SugaredLogger) {
 		if err := c.db.Close(); err != nil {
 			c.log.Errorf("could not close database connection : %s", err)
@@ -75,4 +82,29 @@ func (c client) GetVenue(ctx context.Context, request *api.GetVenueRequest) (*mo
 
 func (c client) CreateVenue(ctx context.Context, request *api.CreateVenueRequest) (*models.Venue, error) {
 	panic("implement me")
+}
+
+func (c *client) migrate() error {
+	driver, err := pgres.WithInstance(c.db.DB, &pgres.Config{})
+	if err != nil {
+		return fmt.Errorf("could not create database driver : %w", err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://migrations",
+		"postgres", driver)
+	if err != nil {
+		return fmt.Errorf("error instantiating migrate : %w", err)
+	}
+
+	version, dirty, _ := m.Version()
+	c.log.Infof("database version %d, dirty %t", version, dirty)
+
+	c.log.Infof("starting migration")
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("an error occurred while syncing the database.. %w", err)
+	}
+
+	c.log.Infof("migration successfully")
+	return nil
 }
